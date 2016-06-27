21ec059b15ff375bf92537881921f7fc8fc293fd
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-06-27 09:36:18 +0200

    Fix test and scenario

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 
-from .test import suite
+from .test_purchase_contract import suite
 
 __all__ = ['suite']
