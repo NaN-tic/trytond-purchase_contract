b438114d59e6a6b323792ea7f3f97e1a1e097ec0
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:12 +0200

    Upgrade test

--- tests/test.py
+++ tests/test.py
@@ -4,23 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('purchase_contract')
-
-    def test0005views(self):
-        'Test views'
-        test_view('purchase_contract')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'purchase_contract'
 
 
 def suite():
@@ -28,5 +19,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_purchase.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='UTF-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
