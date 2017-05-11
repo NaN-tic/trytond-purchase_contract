d5d7cb9d3f490f90f3e13f0b4c43a9f040f97ac8
Raimon Esteve <raimon@nan-tic.com>
2017-05-11 16:21:39 +0200

    Move purchase method to purchase file + locales

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .contract import *
-from .move import *
+from . import contract
+from . import move
+from . import purchase
 
 
 def register():
     Pool.register(
-        PurchaseContract,
-        PurchaseContractLine,
-        Purchase,
-        PurchaseLine,
-        Move,
+        contract.PurchaseContract,
+        contract.PurchaseContractLine,
+        purchase.Purchase,
+        purchase.PurchaseLine,
+        move.Move,
         module='purchase_contract', type_='model')
